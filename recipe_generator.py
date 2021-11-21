4e0561a0af14e8f4c8abfbaf70634a872d608a7c
Zach Karlsen <zachkarlsen@gmail.com>
2021-11-21 17:18:37 -0500

    Create basic wikitree (No Linkages)

--- recipe_generator.py
+++ recipe_generator.py
@@ -3,6 +3,8 @@ from crafting_item import CraftingItem
 import json
 from bs4 import BeautifulSoup
 from treelib import Tree
+import uuid
+from re import sub
 
 fandom.set_wiki('calamitymod')
 
@@ -77,95 +79,189 @@ def get_recipe_prettified(itemname):
     }
     return final_recipe
 
-
-
-
-
-def get_recipe(itemname):
-    def get_recipe(itemname, quantity=1):
-        recipe = {}
-        recipe['name'] = itemname
-        recipe['quantity'] = quantity
+def get_recipe(itemname, quantity=1):
+    recipe = {}
+    recipe['name'] = itemname
+    recipe['quantity'] = quantity
+    try:
+        item_page = fandom.page(title=itemname)
+    except (fandom.error.PageError, KeyError):
+        flip_wiki()
         try:
             item_page = fandom.page(title=itemname)
-        except (fandom.error.PageError, KeyError):
-            flip_wiki()
-            try:
-                item_page = fandom.page(title=itemname)
-            except fandom.error.PageError:
-                return recipe
-        sections = item_page.sections
-        if "Recipe" not in sections:
+        except fandom.error.PageError:
             return recipe
-        html = BeautifulSoup(item_page.html, 'html.parser')
-
-        recipe_table = ''
-        for table in html.find_all('table'):
-            if 'Crafting Station' in table.find_next('tr').find_next('th').contents:
-                recipe_table = table
-                break
-        
-        mode = ''
-        
-        for tr in recipe_table.find_all('tr'):
-            if tr.find('th') is not None:
-                if tr.find('th').find('b') is None:
-                    content = tr.find('th').contents[0]
-                else:
-                    content = tr.find('th').find('b').contents[0]
-                if content == 'Crafting Station':
-                    mode = 'cs'
-                elif content == 'Ingredient(s)':
-                    mode = 'ing'
-                elif content == 'Result':
-                    mode = 'res'
-                continue
-
-            if mode == 'cs':
-                recipe['station'] = tr.find('a')['title']
-                recipe['recipe'] = []
-            elif mode == 'ing':
-                td = tr.find_all('td')
-                name = td[0].find('a')['title'] # ingredient name
-                quantity = int(td[2].contents[0])
-                recipe['recipe'].append(get_recipe(name, quantity))
-            elif mode == 'res':
-                continue
+    sections = item_page.sections
+    if "Recipe" not in sections:
         return recipe
+    html = BeautifulSoup(item_page.html, 'html.parser')
 
-    return get_recipe(itemname)
+    for table in html.find_all('table'):
+        if 'Crafting Station' in table.find_next('tr').find_next('th').contents:
+            recipe_table = table
+            break
+    
+    mode = ''
+    
+    for tr in recipe_table.find_all('tr'):
+        if tr.find('th') is not None:
+            if tr.find('th').find('b') is None:
+                content = tr.find('th').contents[0]
+            else:
+                content = tr.find('th').find('b').contents[0]
+            if content == 'Crafting Station':
+                mode = 'cs'
+            elif content == 'Ingredient(s)':
+                mode = 'ing'
+            elif content == 'Result':
+                mode = 'res'
+            continue
 
-
-
-
-
+        if mode == 'cs':
+            recipe['station'] = tr.find('a')['title']
+            recipe['recipe'] = []
+        elif mode == 'ing':
+            td = tr.find_all('td')
+            name = td[0].find('a')['title'] # ingredient name
+            quantity = int(td[2].contents[0])
+            recipe['recipe'].append(get_recipe(name, quantity))
+        elif mode == 'res':
+            continue
+    return recipe
 
 def create_tree_from_recipe(recipe):
     tree = Tree()
-    root_init = False
-    keys = list(recipe.keys())
+    #create root node
+    if 'station' in recipe:
+        tree.create_node(recipe['name'], identifier=recipe['name'], data=CraftingItem(recipe['name'], 1, recipe['station']))
+    else:
+        tree.create_node(recipe['name'], identifier=recipe['name'], data=CraftingItem(recipe['name'], 1))
 
     def add_nodes(layer, parentname):
-        nonlocal root_init
         for item in layer:
-            if item == 'Station':
-                continue
-            if 'recipe' in layer[item]:
-                if root_init:
-                    node = tree.create_node(item, parent=parentname, data=CraftingItem(item, layer[item]['quantity'], layer[item]['recipe']['Station']))
-                else:
-                    node = tree.create_node(item, data=CraftingItem(item, layer[item]['quantity'], layer[item]['recipe']['Station']))
-                    root_init = True
-                add_nodes(layer[item]['recipe'], node.identifier)
+            if 'station' in item:
+                node = tree.create_node(item['name'], parent=parentname, data=CraftingItem(item['name'], item['quantity'], item['station']))
             else:
-                tree.create_node(item, parent=parentname, data=CraftingItem(item, layer[item]['quantity']))
+                node = tree.create_node(item['name'], parent=parentname, data=CraftingItem(item['name'], item['quantity']))
+            
+            if 'recipe' in item:
+                add_nodes(item['recipe'], node.identifier)
+
+    if 'recipe' in recipe:
+        add_nodes(recipe['recipe'], recipe['name'])
+
+#    tree.show(data_property='printname')
+    return tree
+
+# use get_recipe to 
+def create_wikitree_from_recipe(recipe):
+    tree = create_tree_from_recipe(recipe)
+    properties = '\n'
+    working_wikitree = ''
+    def create_id(item):
+        return str(uuid.uuid4())[:4] + sub('[^A-Z]', '', item)
+
+    def add_layer(layeritems):
+        nonlocal properties, working_wikitree
+        layer_text = ''
+        for node in layeritems:
+            item = tree[node].data
+            item_name = item.get_name()
+            item_id = create_id(item_name)
+            if item.get_quantity() != 1:
+                properties += '|%s_cols=1\n|%s_rows=1\n|%s={{item|%s}} x %d\n' %(item_id, item_id, item_id, item_name, item.get_quantity())
+            else:
+                properties += '|%s_cols=1\n|%s_rows=1\n|%s={{item|%s}}\n' %(item_id, item_id, item_id, item_name)
+            layer_text += '|_|' +  item_id 
+        working_wikitree = layer_text + '|_|#\n' + working_wikitree 
+
+    depth = 0
+    while depth <= tree.depth():
+        layeritems = []
+        for node in tree.expand_tree(mode=Tree.WIDTH):
+            if tree.depth(node) == depth:
+                layeritems.append(node)
+        add_layer(layeritems)
+        depth += 1
+
+    return '{{diagram\n' + working_wikitree + properties + "}}"
+    
 
 
+def create_wikitree_from_item(item):
+    return create_wikitree_from_recipe(get_recipe(item))
 
 
-    add_nodes(recipe, keys[0])
+#print(json.dumps(get_recipe('Fallen Paladin\'s Hammer'), indent=4))
+recipe = {
+    "name": "Fallen Paladin's Hammer",
+    "quantity": 1,
+    "station": "Mythril Anvil",
+    "recipe": [
+        {
+            "name": "Paladin's Hammer",
+            "quantity": 1
+        },
+        {
+            "name": "Pwnagehammer",
+            "quantity": 1,
+            "station": "Mythril Anvil",
+            "recipe": [
+                {
+                    "name": "Pwnhammer",
+                    "quantity": 1
+                },
+                {
+                    "name": "Soul of Might",
+                    "quantity": 3
+                },
+                {
+                    "name": "Soul of Fright",
+                    "quantity": 3
+                },
+                {
+                    "name": "Soul of Sight",
+                    "quantity": 3
+                },
+                {
+                    "name": "Hallowed Bar",
+                    "quantity": 7
+                }
+            ]
+        },
+        {
+            "name": "Ashes of Calamity",
+            "quantity": 5
+        },
+        {
+            "name": "Core of Chaos",
+            "quantity": 5,
+            "station": "Mythril Anvil",
+            "recipe": [
+                {
+                    "name": "Essence of Chaos",
+                    "quantity": 1
+                },
+                {
+                    "name": "Ectoplasm",
+                    "quantity": 1
+                }
+            ]
+        },
+        {
+            "name": "Scoria Bar",
+            "quantity": 5,
+            "station": "Adamantite Forge",
+            "recipe": [
+                {
+                    "name": "Scoria Ore",
+                    "quantity": 5
+                }
+            ]
+        }
+    ]
+}
 
-    tree.show(data_property='printname')
 
-
-print(json.dumps(get_recipe('Fallen Paladin\'s Hammer'), indent=4))
+print(create_wikitree_from_recipe(recipe))
+#create_tree_from_recipe(recipe)
